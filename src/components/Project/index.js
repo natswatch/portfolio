9a56d29a00c48b7e35bf03111a3ac7dc21ff5d40
natswatch <nathaliec@me.com>
2020-12-19 20:01:34 -0800

    overlay text on hover, updated missing links

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -5,19 +5,23 @@ function Project(currentPhoto) {
   const {name, description, link, github, id} = currentPhoto;
   return (
       <>
-        <div className="module lr">
+        <div className="module">
         <img alt={description} 
           src={require(`../../images/${name}-${id}.jpg`).default}
-          className="img-thumbnail mx-1" width="100" key={`${name}${id}`}
+          className="img-thumbnail mx-1" key={`${name}${id}`}
           />
+          <div className="overlay">
           {
             name.length <= 8 ? (
-              <React.Fragment>
-                <a href={github}>{description}</a>
+              <div className="text">
+                <a href={link}>{description}</a><br></br>
+                <a href={github}>Github</a>
+                </div>
               
-              </React.Fragment>
-            ) : ( <a href='#'>{description}</a>)
+              
+            ) : ( <div className="text"><a href='#'>{description}</a></div>)
           }
+          </div>
         </div>
       </>
   );
